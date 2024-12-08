ea4e8304a03b3e83c73ad21cc4ce509d07bccb1c
Gulciya Iltas <igulciya@gmail.com>
2024-12-08 12:08:35 +0300

    Added new projects

---
+++ LoginExample/LoginExample/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  LoginExample
+//
+//  Created by Gülçiya İltaş on 8.12.2024.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- LoginExample/LoginExample/ViewController.swift
+++ LoginExample/LoginExample/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var mailTextField: UITextField!
+    @IBOutlet var registerButtonTapped: UIButton!
+    @IBOutlet var loginButtonTapped: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        view.backgroundColor = .systemBackground
     }
-
-
+    
+    func configureUI() {
+        loginButtonTapped.layer.cornerRadius = 8
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let mail = mailTextField.text, !mail.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty
+        else {
+            print("Lütfen Tüm Alanları Doldurun!")
+            return
+            
+        }
+    }
 }
-
