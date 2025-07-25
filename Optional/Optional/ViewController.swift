61913f341f08a7f76eb7ec0c6cbbb8c6b52c1221
Gulciya Iltas <igulciya@gmail.com>
2025-07-26 01:39:22 +0300

    Add UI for question and answer

---
+++ Optional/Optional/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Optional
+//
+//  Created by Gülçiya İltaş on 20.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
