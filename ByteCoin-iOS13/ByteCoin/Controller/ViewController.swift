171771e46ed6515fef3f383d86cbe53d1454b1c1
Gulciya Iltas <igulciya@gmail.com>
2025-11-17 01:16:35 +0300

    feat: Implement networking call for Bitcoin price

--- ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         coinManager.delegate = self
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
