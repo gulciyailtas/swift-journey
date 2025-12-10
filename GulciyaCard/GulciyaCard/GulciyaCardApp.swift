bcc09370028f4014da2059877ad867bce356c4c0
Gulciya Iltas <igulciya@gmail.com>
2025-12-10 21:28:01 +0300

    feat: Add business card example

---
+++ GulciyaCard/GulciyaCard/GulciyaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GulciyaCardApp.swift
+//  GulciyaCard
+//
+//  Created by Gülçiya İltaş on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GulciyaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
