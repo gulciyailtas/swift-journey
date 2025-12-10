d9e9678233de9caec72c38a3d4e80e9704181256
gulciyailtas <igulciya@gmail.com>
2025-12-10 21:19:31 +0300

    feat: Kartvizit profili olusturuldu (#23)

---
+++ AngelaCard/AngelaCard/AngelaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngelaCardApp.swift
+//  AngelaCard
+//
+//  Created by Gülçiya İltaş on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AngelaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
